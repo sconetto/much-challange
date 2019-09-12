703763eaa164179fdd1db4eee2a23f398e4943d7
João Pedro Sconetto <sconetto.joao@gmail.com>
2019-09-11 23:00:24 -0300

    feat(challenge1): implement challenge 1 functions
    	Adds new methods in Challenge
    
    	Create a main function to execute challenge process
    
    	Update docstrings
    
    	Handles execution errors
    
    Signed-off-by: João Pedro Sconetto <sconetto.joao@gmail.com>

--- challenge1.py
+++ challenge1.py
@@ -10,9 +10,23 @@ class Challenge:
         """Function to initialize the class
 
         Args:
-            self: Self class object
+            self: self class object
         """
+        # Define class API
         self.api = API1()
+        # Initialize a list of SAVED_OBJECTS (used in get_ancestors)
+        self.SAVED_OBJECTS = []
+        # Initialize the total number of objects
+        self.total = 0
+
+    def remove_duplicates(self, objects: list):
+        # Filter list removing duplicates
+        result = [
+            item
+            for index, item in enumerate(objects)
+            if item not in objects[index + 1 :]
+        ]
+        return result
 
     def get_products(self, filename: str):
         """Function to load json file into list to further manipulation
@@ -24,9 +38,6 @@ class Challenge:
             bool: Returns False if the file was not found
             products (list): Return the list with the products
 
-        Raises:
-            FileNotFoundError: If the `filename` file was not found
-
         """
         try:
             file = open(filename, "r")
@@ -37,7 +48,52 @@ class Challenge:
         else:
             return products
 
-    def filter_independent_products(self, products: list):
+    def get_ancestors(self, products: list, product: dict):
+        """Function to get all ancestor for given `product` on a `products` list
+
+        Args:
+            products (list): a list of all products to look up
+            product (dict): the product dictionary which wants to find ancestors
+
+        Returns:
+            bool: False if some error occurred
+            ancestors(list): a list with all ancestors names and ids
+
+        """
+        try:
+            # Get the parent_id from the given product
+            parent_identifier = product["parent_id"]
+            # Get all items
+            found = [item for item in products if item["id"] == parent_identifier]
+            found = self.remove_duplicates(found)
+
+            # Result list
+            ancestors = []
+
+            for item in found:
+                # If item has a parent, search for its ancestors
+                if item["parent_id"] is not None:
+                    for result in self.get_ancestors(products, item):
+                        ancestors.append(result)
+                # For each item of the SAVED_OBJECTS look up for the info of the
+                # ancestor
+                for i in range(len(self.SAVED_OBJECTS)):
+                    if self.SAVED_OBJECTS[i].get("name") == item["name"]:
+                        ancestors.append(
+                            {
+                                "name": item["name"],
+                                "id": self.SAVED_OBJECTS[i].get("id"),
+                            }
+                        )
+
+            ancestors = self.remove_duplicates(ancestors)
+        except Exception as err:
+            logging.error(f"[ERROR] Error while searching ancestors. Traceback: {err}")
+            return False
+        else:
+            return ancestors
+
+    def filter_products(self, products: list):
         """Function to filter all products that does not have parent products
 
         Args:
@@ -47,8 +103,18 @@ class Challenge:
             independent (list): The list of products without parent
 
         """
+        # Get all products that has no parent
         independent = [product for product in products if product["parent_id"] is None]
-        return independent
+        # Get all products that has parent
+        dependent = [
+            product for product in products if product["parent_id"] is not None
+        ]
+        # Sort dependent products by parent_id, so that a child will be always
+        # inserted after the parent
+        dependent = sorted(dependent, key=lambda item: item["parent_id"])
+        # Saves the total of objects
+        self.total = len(independent) + len(dependent)
+        return independent, dependent
 
     def save_independent_products(self, products: list):
         """Function to save products without parents
@@ -59,15 +125,108 @@ class Challenge:
         Returns:
             bool: True if all elements was inserted, otherwise False
 
+        Raises:
+            Exception: If the quantity of products stored isn't the same a the
+            quantity of independents product
+
         """
-        for product in products:
-            response = self.api.create(
-                data={"name": product["name"], "parent_id": None, "ancestors": None}
+        try:
+            for product in products:
+                # Creates each product in the API and saves it to SAVED_OBJECTS
+                # to maintain the new ID for the ancestors
+                response = self.api.create(
+                    data={"name": product["name"], "parent_id": None, "ancestors": None}
+                )
+                logging.info(f"[INFO] Object created: {response}")
+                logging.info(f"[INFO] Storage size: {len(self.api._storage)}")
+                self.SAVED_OBJECTS.append(response)
+
+            if len(products) != len(self.api._storage):
+                raise Exception(
+                    f"Missing objects: Expected {len(products)} "
+                    f"- Stored: {len(self.api._storage)}"
+                )
+        except Exception as err:
+            logging.error(
+                f"[ERROR] Error while saving independent products. Traceback: {err}"
             )
-            logging.info(f"[INFO] Object created: {response}")
-            logging.debug(f"[DEBUG] Storage size: {len(self.api._storage)}")
-        return response
+            return False
+        else:
+            return True
+
+    def save_dependent_products(self, products: list, product_base: list):
+        """Function to save products without parents
+
+        Args:
+            products (list): The list of products that want to be saved
+            product_base (list): The list of all products (used on get_ancestors)
+
+        Returns:
+            bool: True if all elements was inserted, otherwise False
+
+        Raises:
+            Exception: If couldn't get the ancestors
+
+            Exception: If the quantity of products stored isn't the same a the
+            quantity of all products
+
+        """
+        try:
+            for product in products:
+                # Get all ancestors for each product
+                ancestors = self.get_ancestors(product_base, product)
+                if not ancestors:
+                    raise Exception(
+                        "Error while saving dependent products. Couldn't"
+                        " execute get_ancestors(). Verify traceback."
+                    )
+                # Creates each product in the API and saves it to SAVED_OBJECTS
+                # to maintain the new ID for the ancestors
+                response = self.api.create(
+                    data={
+                        "name": product["name"],
+                        "parent_id": ancestors[0]["id"],
+                        "ancestors": list(set([item["name"] for item in ancestors])),
+                    }
+                )
+                logging.info(f"[INFO] Object created: {response}")
+                logging.info(f"[INFO] Storage size: {len(self.api._storage)}")
+                self.SAVED_OBJECTS.append(response)
+            if self.total != len(self.api._storage):
+                raise Exception(
+                    f"Missing objects: Expected {len(product_base)} "
+                    f"- Stored: {len(self.api._storage)}"
+                )
+        except Exception as err:
+            logging.error(
+                f"[ERROR] Error while saving dependent products. Traceback: {err}"
+            )
+            return False
+        else:
+            return True
 
 
-logging.basicConfig(level=logging.DEBUG, format="%(name)s: %(levelname)s - %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(name)s: %(levelname)s - %(message)s")
 
+
+def main():
+    """
+    Main function to execute the process
+    """
+    try:
+        # Instantiate the class and separate objects into two lists
+        challenge = Challenge()
+        product_base = challenge.get_products("product_groups.json")
+        independent, dependent = challenge.filter_products(product_base)
+
+        if not challenge.save_independent_products(independent):
+            raise Exception
+        if not challenge.save_dependent_products(dependent, product_base):
+            raise Exception
+    except Exception as err:
+        logging.error(f"[ERROR] While processing the objects. Traceback: {err}")
+
+
+if __name__ == "__main__":
+    # Run main function
+    main()
