7c184056145fd65db7b1c910955400597d319a8c
João Pedro Sconetto <sconetto.joao@gmail.com>
2019-09-12 22:25:09 -0300

    fix(api3): update bulk create
    	Convert uuid type to str on API3.bulk_create
    
    	Change list to new_objs to save list's objects
    
    Signed-off-by: João Pedro Sconetto <sconetto.joao@gmail.com>

--- api3.py
+++ api3.py
@@ -8,6 +8,6 @@ class API3(API2):
     def bulk_create(self, data: List[dict]):
         """Store multiple objects."""
         self._maybe_crash()
-        new_objs = [{**obj, "id": uuid4()} for obj in data]
-        self._storage.update({obj["id"]: obj for obj in data})
+        new_objs = [{**obj, "id": str(uuid4())} for obj in data]
+        self._storage.update({obj["id"]: obj for obj in new_objs})
         return new_objs
